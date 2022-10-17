1911075c24eed8665bf4109ccf2b3e9a136ec991
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-10-17 15:00:14 +0200

    Addition: Added levelscaling
    Now enemies will spawn on a random place in a room
    and gain additionall hp and strength bases on the current difficulty

---
+++ Assets/scripts/RoomManagement/LevelScaling.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class LevelScaling : MonoBehaviour
+{
+    public float runTime;
+    [SerializeField] private GameObject enemyObject;
+    [SerializeField] private List<GameObject> enemySpawnPoints;
+
+    private float currentDifficulty = 2f;
+    public int minEnemies = 4;
+    public int maxEnemies = 8;
+
+    private int enemyCount = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        runTime = GameObject.FindGameObjectWithTag("Timer").GetComponent<TimerScript>().runTime;
+        SpawnEnemies(minEnemies, maxEnemies);
+    }
+
+
+    /// <summary>
+    /// Spawns enemies at random locations
+    /// </summary>
+    /// <param name="min">Min amount of enemies that needs to be spawned</param>
+    /// <param name="max">Max amount of enemies that needs to be spawned</param>
+
+    private void SpawnEnemies(int min, int max)
+    {        
+        for (float i = Random.Range(min, max +1); i > 0; i--)   
+        {
+            int random = Random.Range(0, enemySpawnPoints.Count) -1;
+            enemySpawnPoints.RemoveAt(random);
+            GameObject instance = Instantiate(enemyObject, enemySpawnPoints[random].transform.position, Quaternion.identity);
+            instance.gameObject.GetComponent<EnemyTestScript>().IncreaseStrengthByDifficulty(currentDifficulty);
+            enemyCount++;    
+        }
+    }
+
+
+
+    private void OnDrawGizmos()
+    {
+        foreach (GameObject spawn in enemySpawnPoints)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(spawn.transform.position, 0.5f);
+        }
+    }
+}
