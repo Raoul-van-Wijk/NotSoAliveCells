ccd9954c0a6aa44b8f1ae5a8de3012981921c1d1
Sven de Goede <Sven.d.goede@gmail.com>
2022-11-18 14:54:24 +0100

    First Sprites

--- Assets/scripts/UIScript.cs
+++ Assets/scripts/UIScript.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class UIScript : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     public Sprite newSprite;
+    public Text timerText;
+    private float secondsCount;
+    private int minuteCount;
+    private int hourCount;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,9 +24,28 @@ public class UIScript : MonoBehaviour
         {
             ChangeSprite();
         }
+
+        UpdateTimerUI();
     }
     void ChangeSprite()
     {
         spriteRenderer.sprite = newSprite;
+    }
+
+    public void UpdateTimerUI()
+    {
+        //set timer UI
+        secondsCount += Time.deltaTime;
+        timerText.text = hourCount + "h:" + minuteCount + "m:" + (int)secondsCount + "s";
+        if (secondsCount >= 60)
+        {
+            minuteCount++;
+            secondsCount = 0;
+        }
+        else if (minuteCount >= 60)
+        {
+            hourCount++;
+            minuteCount = 0;
+        }
     }
 }
